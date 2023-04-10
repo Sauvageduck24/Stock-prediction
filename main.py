3213e9936189fe99594ebb46b27917c307f15764
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-04-10 21:20:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -107,9 +107,8 @@ if logged_in:
         low=values[2].value
         close=values[3].value
 	
-	df=pd.DataFrame([open,high,low,close],columns=['Open','High','Low','Close'])
-	
-	st.dataframe(df)
+        df=pd.DataFrame([open,high,low,close],columns=['Open','High','Low','Close'])
+        st.dataframe(df)
 	
         #st.text(f"Open: {open} High: {high} Low: {low} Close: {close}")
 
