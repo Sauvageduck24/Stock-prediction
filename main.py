f4a1219bc65a552d4a2f3571fb844491a51923fc
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-04-26 18:52:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ if logged_in:
     file = gspread.authorize(creds)
 
     workbook=file.open('Investment 2.0')
+    workbook2=file.open('Investment 2.0 (intra day)')
 		
     st.title('Stock Prediction')
 
@@ -80,6 +81,7 @@ if logged_in:
 
     sheet = workbook.worksheet(f'{selected_stock} DATA')
     sheet2= workbook.worksheet(f'{selected_stock} CALC')
+    sheet3 = workbook2.worksheet(f'{selected_stock} DATA')
 	
     st.subheader('Predicción para el día siguiente')
 
@@ -98,7 +100,7 @@ if logged_in:
     
     else:
         p_open=''
-
+	
     prediction=st.button('Hacer predicción',key='4')
 
     if prediction:
@@ -119,6 +121,10 @@ if logged_in:
         sub_entre_dias=values2[2].value
         anotaciones=values2[4].value
 	
+        values3=sheet.range('G3:J3')
+        high2=values[1].value
+        low2=values[2].value
+	
         st.write('Predicciones para el día')
         	
         df=pd.DataFrame([[open,high,low,close]],columns=['Open','High','Low','Close'])
@@ -131,8 +137,6 @@ if logged_in:
 	
         st.write('Predicciones para 9:01 - 10:01')
 
-        high2,low2='por implementar','por implementar'
-	
         df=pd.DataFrame([[high2,low2]],columns=['Máximo','Mínimo'])
         st.dataframe(df)
 	
