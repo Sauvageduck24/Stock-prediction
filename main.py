1e019c2b3e79641b8a4bc09d936d890701bcaa8d
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-05-12 17:07:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import numpy as np
+import yfinance as yf
 import matplotlib.pyplot as plt
 
 scopes= [
