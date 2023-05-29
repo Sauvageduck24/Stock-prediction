67f076158ec0733cb2ef9ffbb282481cf0cfb5e7
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-05-29 09:03:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -293,9 +293,9 @@ if logged_in:
         ax.fill_between(xs[mask2],high[mask2],mean[mask3], color="green", alpha=0.1)
         ax.fill_between(xs[mask2],mean[mask3],low[mask], color="red", alpha=0.1)
 	
-        if pos_low<pos_high:
-            ax.scatter(pos_high,max(high)+0.01,marker=poss[1],color='r')#,markerfacecolor='red', markersize=13, markeredgecolor="black")
-            ax.scatter(pos_low,min(low)-0.01,marker=poss[0],color='g')#,markerfacecolor='red', markersize=13, markeredgecolor="black")
+        #if pos_low<pos_high:
+            #ax.scatter(pos_high,max(high)+0.01,marker=poss[1],color='r')#,markerfacecolor='red', markersize=13, markeredgecolor="black")
+            #ax.scatter(pos_low,min(low)-0.01,marker=poss[0],color='g')#,markerfacecolor='red', markersize=13, markeredgecolor="black")
 	
         dif=round((100-(min(low)*100)/max(high)),2)
 	
