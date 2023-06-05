226abcab20af7d2398de3e85807f93e59e2d92cb
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-05 09:17:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -281,10 +281,14 @@ if logged_in:
         ax.plot(real,color='black',label='Real Data',alpha=0.85)
         #ax.legend(loc="best")
 	
-        pos_high,=np.where(high==max(high))#[1]
+	try:        
+	    pos_high,=np.where(high==max(high))[0]
+        except:
+            pos_high,=np.where(high==max(high))[0][0]
+	
         pos_low,=np.where(low==min(low))[0]
 	
-        st.write(pos_high.shape)	
+        st.write(pos_high)
         st.write(pos_low)	
 	
         for _,i in enumerate(new_real):
