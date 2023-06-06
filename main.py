566e7d83bfb96ce5c75a48bf76d3d25f05a45213
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-06 15:16:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if logged_in:
         high=sheet3.range('H3:H10')
         low=sheet3.range('I3:I10')
 	
-        data = yf.download(f'{selected_stock}.MC', period=f'1d',interval=f'1m',progress=False)
+        data = yf.download(f'{selected_stock}.MC', period=f'2d',interval=f'1m',progress=False)
 
         now = datetime.now()
         #now=now.replace(tzinfo=datetime.timezone.utc)	
@@ -171,6 +171,7 @@ if logged_in:
         if now.hour+2>9:
             if now.minute>15:
                 pass
+                data=data[:-7*60]
         else:
             data=data[:-9*60]
 	
