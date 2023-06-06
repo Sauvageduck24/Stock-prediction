6ed64e59525b9679e8eb5baeb7f5f0ffc85ac811
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-06 15:35:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -292,8 +292,8 @@ if logged_in:
         pos_high,=np.where(high==max(high))
         pos_low,=np.where(low==min(low))
 	
-        pos_high=pos_high.flat[29]
-        pos_low=pos_low.flat[29]
+        #pos_high=pos_high.flat[29]
+        #pos_low=pos_low.flat[29]
 	
         for _,i in enumerate(new_real):
             new_real[_]=float(i.replace(',','.'))	
