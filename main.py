5495226c3ff0de7e52283b8a60cdbbcf35970291
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-18 18:19:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -342,9 +342,9 @@ if logged_in:
         plt.xlabel("Tiempo (h)")
         plt.ylabel("Precio (€)")
 	
-        new_time=['9:00','9:30','10','10','11','11','12','12','13','13','14','14','15','15','16','16']
+        new_time=['9','10','11','12','13','14','15','16']
 	
-        plt.xticks(np.arange(0, len(low_mins), 30),new_time)
+        plt.xticks(np.arange(0, len(low_mins), 60),new_time)
 	
         ax.legend(loc="best")
 	
