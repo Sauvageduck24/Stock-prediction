c1892c07d58fddbad3e89918b3e6e7d731707f61
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-26 10:38:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ db_data=sheet_db.range("B2:E1000")
 usernames=db_data[0];passwords=db_data[1];emails=db_data[2];end_times=db_data[3]
 
 st.write(usernames)
+st.write(passwords)
 
 usernames_f=[];passwords_f=[];end_times_f=[];emails_f=[]
 
