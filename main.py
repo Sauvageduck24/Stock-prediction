7148c1b49450cde8b958d2b2685dff958065031b
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-27 12:13:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -105,10 +105,10 @@ if authentication_status:
 	
     col1,col2,col3=st.columns([1,1,1])
 
-    with col2:
+    with col1:
         st.write(f"Has iniciado sesión como: {name.upper()}")
 
-    with col3:
+    with col2:
         authenticator.logout('Logout', 'main', key='unique_key')
         st.title('Market Master')
 
