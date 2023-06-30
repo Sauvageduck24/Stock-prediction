e1fda34f1a05e5bfb8282abf53eec0bc41d4e3ed
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-06-30 11:31:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,10 @@ if authentication_status:
         z=[1,2,3,4,5,6,7,8]
 
         for X, Y, Z in zip(xs[mask], high[mask], z):
-            ax.annotate('{}'.format(Z), xy=(X,Y), xytext=(-5, 5), ha='right',textcoords='offset points',color='white')	
+            ax.annotate('{}'.format(Z), xy=(X,Y), xytext=(-5, 5), ha='right',textcoords='offset points',color='white')
+
+        for X, Y, Z in zip(xs[mask], low[mask], z):
+            ax.annotate('{}'.format(Z), xy=(X,Y), xytext=(-5, 5), ha='right',textcoords='offset points',color='white')
 	    
         ax.plot(real,color='white',label='Real Data',alpha=0.9)
 
