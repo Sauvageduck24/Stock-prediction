09e71c623b7d8774a9949db120b09bdc0c2bde07
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-07-06 10:26:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import warnings
 from PIL import Image
-from matplotlib import font_manager
-
-#font_dir=['Dense-Regular.otf']
-#font_files = font_manager.findSystemFonts(fontpaths=font_dir,fontext='otf')
-
-#for font_file in font_files:
-    #font_manager.fontManager.addfont(font_file)
-
-#plt.rcParams['font.family'] = 'Dense'
 
 image = Image.open('market-master-logo.png')
 
