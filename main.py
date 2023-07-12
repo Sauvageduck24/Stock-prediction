da800c05105e5eb69b56d16b7e201839afa183f9
Sauvageduck24 <74764572+Sauvageduck24@users.noreply.github.com>
2023-07-12 09:47:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -490,7 +490,8 @@ if authentication_status:
         ax.plot(real_l,color='white',alpha=0.9)
 
         ax.fill_between(time,real_l_, color="red", alpha=0.2)
-        ax.fill_between(time,real_h_,1, color="green", alpha=0.2)
+        #ax.fill_between(time,real_h_,1, color="green", alpha=0.2)
+        ax.fill_between(time, real_h_, 1, color='blue', alpha=.1)
 	    
         z_h=[]
         z_l=[]
